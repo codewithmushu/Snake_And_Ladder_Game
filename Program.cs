73b19d7cc59d0cf722b1418eed6b873d5c04c5fc
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-13 23:29:19 +0530

    [Muskan] Add : Define some variables for game.

--- Program.cs
+++ Program.cs
@@ -12,19 +12,19 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("Welcome to Game World");
 
-             int[] snakes = { 46, 48, 52, 59, 64, 68, 69, 83, 89, 93, 98 };
-             int[] Ladders = { 8, 19, 21, 28, 38, 43, 50, 54, 61, 62, 66 };
+            //Define the snakes and ladders as arrays.
+            int[] snakes = { 46, 48, 52, 59, 64, 68, 69, 83, 89, 93, 98 };
+            int[] Ladders = { 8, 19, 21, 28, 38, 43, 50, 54, 61, 62, 66 };
 
-            //int[] snakes = { 17 , 54, 62, 98 };
-            //int[] Ladders = { 3, 24, 42, 72 };
- 
             //Define the player position
             int PlayerPosition = 0;
 
 
-            // UC1_SinglePlayer.StartPosition0();
-            // UC2_PlayerRollsTheDie.Random1To6(PlayerPosition);
-            UC3_NoPlay.NoPlay_Ladder_Snake(PlayerPosition, snakes, Ladders);
+
+
+           // UC1_SinglePlayer.StartPosition0(PlayerPosition , snakes,Ladders);
+             UC2_PlayerRollsTheDie.Random1To6(PlayerPosition ,snakes, Ladders);
+            //UC3_NoPlay.NoPlay_Ladder_Snake(PlayerPosition, snakes, Ladders);
         }
     }
 }
