185f360ccce4da33010ce08fd8f6098a6bed4230
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-13 23:33:45 +0530

    [Muskan] Add : Snake and Ladder game played with single player at start position 0.

---
+++ UC1_SinglePlayer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace SnakeAndLadder
+{
+    public class UC1_SinglePlayer
+    {
+        public static void StartPosition0(int PlayerPosition,int[] snakes,int[] Ladders)
+        {
+            
+            while (PlayerPosition < 100)
+            {
+                Random random = new Random();
+                int rolls = random.Next(1, 7);
+                //_ = random.Next(1, 7);
+                PlayerPosition += rolls;
+                Console.WriteLine("You rolled a {0} and moved to position {1} ", rolls, PlayerPosition);
+
+
+
+                //Check for snakes and ladders
+                if (Array.IndexOf(snakes, PlayerPosition) >= 0)
+                {
+                    PlayerPosition -= rolls;
+                    Console.WriteLine("Oh no ! you landed on a snake and slide down to position {0}", PlayerPosition);
+
+                }
+                else if (Array.IndexOf(Ladders, PlayerPosition) >= 0)
+                {
+                    PlayerPosition += rolls;
+                    Console.WriteLine("Wow! you landed on a ladder and moved ahead to position {0} ", PlayerPosition);
+
+                }
+
+            }
+
+            Console.WriteLine("Congratulations! you won");
+
+            
+
+
+
+
+
+        }
+    }
+}
