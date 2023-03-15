9a6acd81367876962a8fc3ec1847cb34821d3825
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-15 17:05:21 +0530

    [Muskan] Added : Calling two players program.

--- Program.cs
+++ Program.cs
@@ -17,8 +17,8 @@ namespace SnakeAndLadder
             int[] Ladders = { 8, 19, 21, 28, 38, 43, 50, 54, 61, 62, 66 };
 
             //Define the player position
-             int PlayerPosition = 0;
-             int roll = 0;
+            // int PlayerPosition = 0;
+            // int roll = 0;
 
 
 
@@ -28,8 +28,8 @@ namespace SnakeAndLadder
             // UC3_NoPlay.NoPlay_Ladder_Snake(PlayerPosition, snakes, Ladders);
             // UC4_RepeatTillWinning.PlayerRestart(PlayerPosition,snakes,Ladders);
             // UC5_ExactWinninngPosition100.Playergetsexactnumtoadd100(PlayerPosition,snakes,Ladders);
-            UC6_NoOfTimeDicePlayed.DisplayNoOfTimeDicePlayed(PlayerPosition, snakes, Ladders, roll);
-            //UC7_TwoPlayers.DisplayWhoWon(snakes,Ladders);
+            //UC6_NoOfTimeDicePlayed.DisplayNoOfTimeDicePlayed(PlayerPosition, snakes, Ladders, roll);
+            UC7_TwoPlayers.DisplayWhoWon(snakes,Ladders);
         }
     }
 }
